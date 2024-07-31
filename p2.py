1469ca5e6c5c3ed372a6a0d5c63da8a2a2414bb6
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-07-31 12:05:25 +0700

    eiei

--- p2.py
+++ p2.py
@@ -5,3 +5,17 @@ def reverse_strings():
             break
         print(line[::-1])
 reverse_strings()
+
+
+def reverse_strings():  ##  อันนี้คือรับเรื่อยยๆ
+    try:
+        while True:
+            line = input()
+            print(line[::-1])
+    except EOFError:
+        pass
+
+reverse_strings()
+
+
+# python p2.py < test.txt > output.txt เอาไว้test
